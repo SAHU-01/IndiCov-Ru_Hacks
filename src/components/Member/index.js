c5106cc1e6b86c63a584a85cafe248c7ee96cb2d
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2021-05-02 20:21:31 +0530

    Add files via upload

---
+++ src/components/Member/index.js
@@ -0,0 +1,42 @@
+import React,{useState, useEffect} from "react";
+
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+//Importing Components
+
+import style from "./style";
+import Links from '../Links/index'
+const useStyles = makeStyles(style);
+
+function Member({ name, occ1, occ2, image, linkedIn, instagram, github, website}) {
+  const classes = useStyles();
+  const [IsDesktop, setIsDesktop] = useState(window.innerWidth > 900)
+  const updateMedia = () => {
+    setIsDesktop(window.innerWidth > 900);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+  return (
+    <div className={classes.member}>
+
+      <div className={ IsDesktop ? classes.infoHolder : classes.infoHolder2}>
+        <div className={classes.imageWrapper}>
+          <img className={IsDesktop ? classes.image : classes.image2 } src={image} alt="Member" />
+        </div>
+        <div className={classes.infoBox}>
+          <Typography variant="h5" className={classes.title}>{name}</Typography>
+          <Typography variant="h6" className={classes.role}>{occ1}</Typography>
+          <Typography variant="subtitle2" className={classes.description}>{occ2}</Typography>
+          <div style={{display:'flex'}}>
+              <Links linkedIn={linkedIn} instagram={instagram} github={github} website={website} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Member;
